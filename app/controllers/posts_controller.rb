82096724dca52e8c40e8ad7bd452f0744ebb7910
Bian Klančnik <bian.klancnik@gmail.com>
2018-03-27 08:10:46 +0000

    Osnovna stran

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,26 @@ class PostsController < ApplicationController
         @post = Post.new
     end
     
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+    
     def create
         @post = Post.new(post_params) 
         
